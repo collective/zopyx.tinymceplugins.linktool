3229c5d7a4996a93f0e7a633da8a9ff89a3825a4
Andreas Jung <yet@gmx.de>
2011-11-07 06:06:39 -0800

    added

---
+++ zopyx/tinymceplugins/linktool/interfaces.py
@@ -0,0 +1,7 @@
+from zope.interface import Interface
+
+class ILinkable(Interface):
+    """ Marker interface for content types being considered
+        to be used with the LinkTool.
+    """
+    pass
---
+++ zopyx/tinymceplugins/linktool/subscribers/linktool.py
@@ -0,0 +1,27 @@
+#####################################################################
+# zopyx.tinymceplugins.linktool
+# (C) 2011, ZOPYX Limited, D-72070 Tuebingen. All rights reserved
+#####################################################################
+
+from ..interfaces import ILinkable
+from ..transforms import Transformer
+
+_marker = object
+
+def postEdit(event):
+   
+    obj = event.object
+    if not ILinkable.providedBy(obj):
+        return
+
+    field = obj.Schema().getField('text')
+    if not field:
+        return
+
+    # only modify text/html text fields (omit fields with reST etc.)
+    if field.getContentType(obj) in ('text/html',):
+        T = Transformer(('addUUIDs',)
+        html = T(obj.getText(), input_encoding='utf-8')
+        obj.setText(html)
+        obj.setContentType('text/html')
+        obj.getField('text').setContentType(obj, 'text/html')
